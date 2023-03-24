4ecd2130e44f61bd225120cc32a8d5d17b2dcf9c
spectre2727 <gsoldano27@outlook.com>
2023-03-24 15:31:07 +0100

    [project] wildfly-bootable

---
+++ wildfly-bootable/src/main/java/spc/RecordObject.java
@@ -0,0 +1,24 @@
+package spc;
+
+public class RecordObject {
+
+    private String id;
+    private String origin;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+}
---
+++ wildfly-bootable/webcontent/main.js
@@ -0,0 +1,1 @@
+document.getElementById("js").innerHTML = "Hello from Javascript";
